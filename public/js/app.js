32efd62dd1118833b42fdb08c735f5ed9f47fe8a
Dennis <okoyedennis7@gmail.com>
2022-12-27 11:45:33 +0100

    Removed unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Js file is loading");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
